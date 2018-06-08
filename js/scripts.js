cd1ad8243024d6b2c09faa7d6acb9328c579be8a
William Kinzig <wippawill2@gmail.com>
2018-06-08 12:26:36 -0700

    make first function receive user input and convert the string to an array, then return the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
 $(document).ready(function() {
   $("form#beep").submit(function(event) {
     event.preventDefault();
+
     var year = parseInt($("input#userNumberInput").val());
-    var result = numCheck(year);
-    $("#result").text(result);
+
+    //var result = numCheck(year);//GARBAGE
+
+    var output = makeArray(year);// send number to makeArray function
+
+    $("#result").text(output);// OUTPUT TO USER
   });
 });
 
-function numCheck(numberFromUser)
+
+//GARBAGE
+// function numCheck(numberFromUser)
+// {
+//     return numberFromUser;
+// }
+
+
+function makeArray(x)
 {
-    return "bodfsjao";
+  arrayOut = x.toString().split('');
+  return arrayOut;
 }
