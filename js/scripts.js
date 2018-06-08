81223fb174c5e3e18baf48fd95e348065d2ddb92
William Kinzig <wippawill2@gmail.com>
2018-06-08 13:45:29 -0700

    fix the if statement that is inside the checkResults function

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,15 @@ $(document).ready(function() {
 
     var numberToCheck = parseInt($("input#userNumberInput").val());
 
-    //var result = numCheck(year);//GARBAGE
-
     var arrayFromNum = makeArray(numberToCheck);// send number to makeArray function
 
     var resultArray = checkResult(arrayFromNum);
 
-    var resultToString = resultArray.toString();
+    //var resultToString = resultArray;
 
-    $("#result").text(resultToString);// OUTPUT TO USER
+    $("#result").text(resultArray);// OUTPUT TO USER
   });
 });
-
-
-//GARBAGE
-// function numCheck(numberFromUser)
-// {
-//     return numberFromUser;
-// }
 
 
 function makeArray(x)
@@ -30,28 +21,30 @@ function makeArray(x)
   return arrayOut;
 }
 
-function checkResult(arrayIn)
-{
-  for(var index = 0; index < arrayIn.length; index += 1){
+// function checkResult(arrayIn)
+// {
+//   for(var index = 0; index < arrayIn.length; index += 1){
+//
+//
+//     var cond_0 = "Beep!"
+//     if (arrayIn[index] === 0){
+//       arrayIn = [];
+//       arrayIn.push(cond_0)
+//       return arrayIn;
+//     }
+//   }
+// }
 
-    var cond_0 = "Beep!"
-    if (arrayIn[index] === 0){
-      arrayIn = [];
-      arrayIn.push(cond_0)
-      return arrayIn;
+
+function checkResult(x)
+{
+  var stringOutput = "";
+  for(var i=0; i<x.length; i++)
+  {
+    if(x[i] == 0){
+      stringOutput = "jfsalk;";
+      return stringOutput;
     }
   }
+  return false;
 }
-
-
-
-// function checkResult(x)
-// {
-//   var count = 0;
-//   for(var i=0; i<x.length; i++)
-//   {
-//     if
-//     count += 1;
-//   }
-//   return count;
-// }
